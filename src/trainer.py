a3b7afe0edd098d9eef14d5facfd4a5faf8bb9dd
lockcept <lockcept@gmail.com>
2024-11-26 01:50:38 +0900

    load with weights_only

--- src/trainer.py
+++ src/trainer.py
@@ -123,5 +123,5 @@ class NNetWrapper:
         if not os.path.exists(filepath):
             raise BaseException("No model in path {}".format(filepath))
         map_location = None if args.cuda else "cpu"
-        checkpoint = torch.load(filepath, map_location=map_location)
+        checkpoint = torch.load(filepath, map_location=map_location, weights_only=True)
         self.nnet.load_state_dict(checkpoint["state_dict"])
